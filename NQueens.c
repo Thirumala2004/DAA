71c1be008d682d15b2ec66cd0898a7284a166d93
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2025-04-02 09:28:23 +0530

    Update NQueens.c

--- NQueens.c
+++ NQueens.c
@@ -1,62 +1,58 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
-int board[100],count;
+int board[20],count;
 int main()
 {
-    int n,i,j;
-    printf("Enter number of Queens:");
-    scanf("%d",&n);
-    queen(1,n);
-    return 0;
+ int n,i,j;
+ void queen(int row,int n);
+ printf("\n\nEnter number of Queens:");
+ scanf("%d",&n);
+ queen(1,n);
+ return 0;
 }
 void print(int n)
 {
-    int i,j;
-    printf("\n\nSolution %d:\n\n",++count);
-    for(i=1;i<=n;++i)
-    printf("\t%d",i);
-    for(i=1;i<=n;++i)
-    {
-        printf("\n\n%d",i);
-        for(j=1;j<=n;++j) //for nxn board
-        {
-            if(board[i]==j)
-                printf("\tQ"); //queen at i,j position
-            else
-                printf("\t-"); //empty slot
-        }
-    }
+ int i,j;
+ printf("Solution %d:\n\n",++count);
+ for(i=1;i<=n;++i)
+  printf("\t%d",i);
+ for(i=1;i<=n;++i)
+ {
+  printf("\n\n%d",i);
+  for(j=1;j<=n;++j) //for nxn board
+  {
+   if(board[i]==j)
+    printf("\tQ"); //queen at i,j position
+   else
+    printf("\t-"); //empty slot
+  }
+ }
 }
 int place(int row,int column)
 {
-    int i;
-    for(i=1;i<=row-1;++i)
-    {
-        if(board[i]==column)
-            return 0;
-        else
-            if(abs(board[i]-column)==abs(i-row))
-                return 0;
-    }
-    return 1; //no conflicts
+ int i;
+ for(i=1;i<=row-1;++i)
+ {
+  if(board[i]==column)
+   return 0;
+  else
+   if(abs(board[i]-column)==abs(i-row))
+    return 0;
+ }
+ return 1;
 }
 void queen(int row,int n)
 {
-    int column;
-    for(column=1;column<=n;++column)
-    {
-        if(place(row,column))
-        {
-            board[row]=column; //no conflicts so place queen
-            if(row==n) //dead end
-                print(n); //printing the board configuration
-            else //try queen with next position
-                queen(row+1,n);
-        }
-    }
+ int column;
+ for(column=1;column<=n;++column)
+ {
+  if(place(row,column))
+  {
+   board[row]=column; 
+   if(row==n) 
+    print(n);  
+   else 
+    queen(row+1,n);
+  }
+ }
 }
-
-
-
-
